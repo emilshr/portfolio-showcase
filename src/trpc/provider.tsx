f4a0923129ca9ce2f6c8622feae76f798c75a150
Emil Sharier <emil.sharier.dev@gmail.com>
2023-10-08 10:48:55 +0530

    Removing test route, adding next auth middleware

--- src/trpc/provider.tsx
+++ src/trpc/provider.tsx
@@ -11,8 +11,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
  */
 const getBaseUrl = () => {
   if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+  if (process.env.BASE_URL) return `https://${process.env.BASE_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT}`; // dev SSR should use localhost
 };
 
 export default function Provider({ children }: PropsWithChildren) {
