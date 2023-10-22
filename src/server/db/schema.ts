14ebd11c6098b463f3cfd4c5e222610929431872
Emil Sharier <emil.sharier.dev@gmail.com>
2023-10-22 22:25:55 +0530

    Clean up, Adding shadcn

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,32 +1,12 @@
-import { relations, sql } from "drizzle-orm";
+import { relations } from "drizzle-orm";
 import {
-  bigint,
-  index,
   primaryKey,
   text,
   timestamp,
-  varchar,
   integer,
   pgTable,
 } from "drizzle-orm/pg-core";
 import { type AdapterAccount } from "next-auth/adapters";
-
-export const posts = pgTable(
-  "post",
-  {
-    id: bigint("id", { mode: "number" }).primaryKey(),
-    name: varchar("name", { length: 256 }),
-    createdById: varchar("createdById", { length: 255 }).notNull(),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt"),
-  },
-  (example) => ({
-    createdByIdIdx: index("createdById_idx").on(example.createdById),
-    nameIndex: index("name_idx").on(example.name),
-  }),
-);
 
 export const users = pgTable("user", {
   id: text("id").notNull().primaryKey(),
